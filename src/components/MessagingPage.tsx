19f4a7afccfa1672ff40d5e34c1852f49ee3aab6
VARSHA G <varshasekar0706@gmail.com>
2025-06-25 00:20:33 +0530

    chat

--- src/components/MessagingPage.tsx
+++ src/components/MessagingPage.tsx
@@ -9,23 +9,24 @@ interface Profile {
   created_at?: string;
 }
 
-
-
-
-
 export default function MessagingPage() {
   const [selectedUser, setSelectedUser] = useState<Profile | null>(null);
 
   return (
-    <div className="flex h-screen bg-black">
-      <div className="w-1/4 border-r border-white">
+    <div className="flex flex-col md:flex-row h-screen bg-black overflow-hidden">
+      {/* Sidebar for User List */}
+      <div className="w-full md:w-1/4 border-b md:border-b-0 md:border-r border-white max-h-[40vh] md:max-h-full overflow-y-auto">
         <UserList onSelectUser={setSelectedUser} />
       </div>
-      <div className="w-3/4">
+
+      {/* Chat Area */}
+      <div className="w-full md:w-3/4 flex-1">
         {selectedUser ? (
           <ChatBox selectedUser={selectedUser} />
         ) : (
-          <div className="text-white p-4">Select a user to start chatting.</div>
+          <div className="text-white p-4 flex items-center justify-center h-full text-center text-lg">
+            Select a user to start chatting.
+          </div>
         )}
       </div>
     </div>
