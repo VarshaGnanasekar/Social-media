5bce03a0a44f9f6e207ae6e436af419247c54a6c
VARSHA G <varshasekar0706@gmail.com>
2025-06-27 00:24:13 +0530

    message

--- src/components/MessagingPage.tsx
+++ src/components/MessagingPage.tsx
@@ -51,10 +51,9 @@ export default function MessagingPage() {
       {/* Show user list when no user is selected */}
       {!selectedUser ? (
         <div className="flex flex-col w-full h-full">
-          {/* Header */}
-          <div className="p-3 border-b border-[#222] flex items-center justify-between">
-            <h1 className="text-xl font-bold text-white">Messages</h1>
-          </div>
+<h2 className="text-4xl max-sm:text-2xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-cyan-400  bg-clip-text text-transparent">
+        Get to know each others
+      </h2>
           
           {/* Search bar */}
           <div className="relative px-3 py-2 border-b border-[#222]">
