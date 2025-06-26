0fe291e60454255f52133abcfe520216f1f1cc5e
VARSHA G <varshasekar0706@gmail.com>
2025-06-26 22:38:41 +0530

    delete post

--- src/components/FollowUsersPage.tsx
+++ src/components/FollowUsersPage.tsx
@@ -80,20 +80,7 @@ export const FollowUsersPage = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="w-full px-4 py-2 rounded-md bg-[#1f1f1f] text-white border border-[#333] focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <svg
-          className="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+        
       </div>
 
       {loading ? (
