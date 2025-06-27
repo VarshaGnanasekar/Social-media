7a880b77cf70e3c521c86ff4d04761a29fff66dc
VARSHA G <varshasekar0706@gmail.com>
2025-06-27 18:36:33 +0530

    heading

--- src/pages/GamingHub.tsx
+++ src/pages/GamingHub.tsx
@@ -3,35 +3,121 @@ import GameCard from "../components/GameCard"
 
 export default function GamingHub() {
   const games = [
-    {
-      id: 1,
-      title: "2048 Classic",
-      genre: "Puzzle",
-      image: "https://picsum.photos/seed/2048/300/200",
-      playLink: "/games/2048",
-    },
-    {
-      id: 2,
-      title: "Tic Tac Toe",
-      genre: "Strategy",
-      image: "https://picsum.photos/seed/tictactoe/300/200",
-      playLink: "/games/tictactoe",
-    },
-    {
-      id: 3,
-      title: "Typing Race",
-      genre: "Skill",
-      image: "https://picsum.photos/seed/typing/300/200",
-      playLink: "/games/typing",
-    },
-    {
-      id: 4,
-      title: "Memory Flip",
-      genre: "Memory",
-      image: "https://picsum.photos/seed/memory/300/200",
-      playLink: "/games/memory",
-    },
-  ];
+  // Puzzle Games
+  {
+    id: 1,
+    title: "2048 Classic",
+    genre: "Puzzle",
+    image: "https://img.itch.zone/aW1nLzEzNDg2MjAucG5n/original/BW7m%2Fn.png",
+    playLink: "https://play2048.co/",
+  },
+  {
+    id: 2,
+    title: "Tetris",
+    genre: "Puzzle",
+    image: "https://img.itch.zone/aW1nLzUxOTUwNjAucG5n/original/3%2FQw%2Fx.png",
+    playLink: "https://tetris.com/play-tetris",
+  },
+  {
+    id: 3,
+    title: "Sudoku",
+    genre: "Puzzle",
+    image: "https://img.itch.zone/aW1nLzEyNjc2MTYucG5n/original/6%2F8qYV.png",
+    playLink: "https://sudoku.com/",
+  },
+
+  // Strategy Games
+  {
+    id: 4,
+    title: "Tic Tac Toe",
+    genre: "Strategy",
+    image: "https://img.itch.zone/aW1nLzU4NDU1OTcucG5n/original/%2B%2Fv%2B%2Ft.png",
+    playLink: "https://playtictactoe.org/",
+  },
+  {
+    id: 5,
+    title: "Chess",
+    genre: "Strategy",
+    image: "https://img.itch.zone/aW1nLzYxMjQ3NDQucG5n/original/7%2F3Q%2F3.png",
+    playLink: "https://www.chess.com/play/online",
+  },
+  {
+    id: 6,
+    title: "Checkers",
+    genre: "Strategy",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MDMucG5n/original/0%2Fw%2F%2FQ.png",
+    playLink: "https://www.mathsisfun.com/games/checkers.html",
+  },
+
+  // Skill Games
+  {
+    id: 7,
+    title: "Typing Race",
+    genre: "Skill",
+    image: "https://img.itch.zone/aW1nLzYwNDQ2NjAucG5n/original/0%2F%2FJ4%2F.png",
+    playLink: "https://play.typeracer.com/",
+  },
+  {
+    id: 8,
+    title: "Aim Trainer",
+    genre: "Skill",
+    image: "https://img.itch.zone/aW1nLzYyMjQ0MTYucG5n/original/%2F%2F%2Fw%2F1.png",
+    playLink: "https://aimtrainer.io/",
+  },
+  {
+    id: 9,
+    title: "Reaction Time",
+    genre: "Skill",
+    image: "https://img.itch.zone/aW1nLzYyMjQ0MTcucG5n/original/9%2F%2F%2F%2Fk.png",
+    playLink: "https://humanbenchmark.com/tests/reactiontime",
+  },
+
+  // Memory Games
+  {
+    id: 10,
+    title: "Memory Flip",
+    genre: "Memory",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MDYucG5n/original/5%2F%2F%2F%2F3.png",
+    playLink: "https://www.memozor.com/memory-games/for-adults",
+  },
+  {
+    id: 11,
+    title: "Simon Game",
+    genre: "Memory",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MDcucG5n/original/9%2F%2F%2F%2F7.png",
+    playLink: "https://www.mathsisfun.com/games/simon.html",
+  },
+  {
+    id: 12,
+    title: "Pattern Memory",
+    genre: "Memory",
+    image: "https://img.itch.zone/aW1nLzYyMjQ0MTgucG5n/original/9%2F%2F%2F%2F9.png",
+    playLink: "https://humanbenchmark.com/tests/memory",
+  },
+
+  // Classic Arcade
+  {
+    id: 13,
+    title: "Snake Game",
+    genre: "Arcade",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MDkucG5n/original/9%2F%2F%2F%2F9.png",
+    playLink: "https://playsnake.org/",
+  },
+  {
+    id: 14,
+    title: "Pac-Man",
+    genre: "Arcade",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MTAucG5n/original/9%2F%2F%2F%2F9.png",
+    playLink: "https://www.pacman.com/",
+  },
+  {
+    id: 15,
+    title: "Space Invaders",
+    genre: "Arcade",
+    image: "https://img.itch.zone/aW1nLzU4NDU2MTEucG5n/original/9%2F%2F%2F%2F9.png",
+    playLink: "https://www.spaceinvaders.com/",
+  }
+];
 
   return (
     <div className="min-h-screen bg-[#050505] text-white px-4 py-8">
