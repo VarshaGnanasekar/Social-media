6a22f1a7757427f49690700c2573c112dbc09d08
VARSHA G <varshasekar0706@gmail.com>
2025-07-10 00:43:48 +0530

    tweet section

--- src/components/TweetItem.tsx
+++ src/components/TweetItem.tsx
@@ -14,7 +14,8 @@ export const TweetItem = ({ tweet }: Props) => {
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
-      className="bg-gray-900 rounded-xl border border-gray-800 p-5 hover:border-gray-700 transition-colors duration-300"
+      className="bg-black rounded-xl border border-gray-800 p-5 hover:border-gray-700 transition-colors duration-300"
+
     >
       <div className="flex gap-3">
         {/* Avatar */}
